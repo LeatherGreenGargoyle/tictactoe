cf44a811485cc8e94c10dde23477784ae0de63f3
leathergreengargoyle <avtran23@gmail.com>
2017-06-12 16:25:25 -0700

    [feat]Create newGame constructor, placePiece method w/o winchecks

--- app.js
+++ app.js
@@ -10,3 +10,48 @@ vertical win checker
 diagonal win checker
 also checks for stalemate - if no win checkers happen, and we've hit 9 pieces (counter) then return stalemate
 */
+
+const newGame = () => {
+  this.board = [['','',''],['','',''],['','','']];
+
+  this.currPc = 'X';
+
+  this.placePiece = (moveIdx) => {
+    if (moveIdx === 1) {
+      this.board[0][0] = this.currPc;
+    }
+    if (moveIdx === 2) {
+      this.board[0][1] = this.currPc;
+    }
+    if (moveIdx === 3) {
+      this.board[0][2] = this.currPc;
+    }
+    if (moveIdx === 4) {
+      this.board[1][0] = this.currPc;
+    }
+    if (moveIdx === 5) {
+      this.board[1][1] = this.currPc;
+    }
+    if (moveIdx === 6) {
+      this.board[1][2] = this.currPc;
+    }
+    if (moveIdx === 7) {
+      this.board[2][0] = this.currPc;
+    }
+    if (moveIdx === 8) {
+      this.board[2][1] = this.currPc;
+    }
+    if (moveIdx === 9) {
+      this.board[2][2] = this.currPc;
+    }
+    
+    // execute win checkers
+
+    if (this.currPc === 'X') {
+      this.currPc = 'O';
+    } else {
+      this.currPc = 'X';
+    }
+  }
+
+}
